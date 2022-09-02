e31ac59192236b737931588d29a621590d427681
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-09-02 13:21:31 +0900

    add Class

--- src/model/Android.java
+++ src/model/Android.java
@@ -2,4 +2,7 @@ package model;
 
 public class Android extends Device {
 
+	public Android() {
+		System.out.println("Hello Android");
+	}
 }
--- src/model/Device.java
+++ src/model/Device.java
@@ -1,5 +1,9 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Device {
 
+	ArrayList<Software> softwares = new ArrayList<>();
+	
 }
