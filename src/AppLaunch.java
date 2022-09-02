91b5792f8c0006d4eadae974c833e545d4eefc66
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-09-02 14:02:04 +0900

    add app instance

--- src/AppLaunch.java
+++ src/AppLaunch.java
@@ -1,9 +1,17 @@
 import model.Android;
+import model.Camera;
+import model.Editor;
+import model.Phone;
 
 public class AppLaunch {
 
 	public static void main(String[] args) {
 		Android android = new Android();
+		
+		Camera camera = new Camera();
+		Phone phone = new Phone();
+		Editor editor = new Editor();
+		
 	}
 
 }
