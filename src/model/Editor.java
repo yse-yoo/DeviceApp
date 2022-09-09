7831e782aac1d92eb7f41bd5e0adb2ec2aedd1d3
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-09-09 10:03:03 +0900

    add FileWriter

--- src/model/Editor.java
+++ src/model/Editor.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Editor extends Software {
 
 	final static String appName = "Editor App"; 
@@ -10,5 +13,16 @@ public class Editor extends Software {
 	
 	public void save(String path, String message) {
 		System.out.println("save: " + path);
+		
+		try {
+			FileWriter writer = new FileWriter(path);
+			writer.write(message);
+			writer.close();
+
+			System.out.println("ファイルを保存しました");
+		} catch (IOException e) {
+			System.out.println("ファイルが書き込めませんでした");
+		}
+		
 	}
 }
